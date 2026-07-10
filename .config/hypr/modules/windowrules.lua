487fa93d66820a719fe4957e66866f6428b2a9e0
nafuia <aryavirsingh8g.7@gmail.com>
2026-07-10 23:15:22 +0530

    comments removal

--- .config/hypr/modules/windowrules.lua
+++ .config/hypr/modules/windowrules.lua
@@ -29,20 +29,3 @@ hl.window_rule({
 
     no_focus = true,
 })
-
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
---hl.window_rule({
---  name  = "move-hyprland-run",
---  match = { class = "hyprland-run" },
-
---  move  = "20 monitor_h-120",
---  float = true,
---})
