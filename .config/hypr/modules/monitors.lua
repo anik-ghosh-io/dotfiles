571ff2d6086b71964c67860f160a6b5f17340d62
Anik Ghosh <anikghosh7887@gmail.com>
2026-07-11 14:31:59 +0530

    battery check

--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -4,8 +4,8 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "1.25",
+    output   = "eDP-1",
+    mode     = "1920x1080@60",
+    position = "0x0",
+    scale    = "1.1",
 })
